a17a44b1d5f453296e72d04d4ef6e322179af7e0
vlad <78017035+vladprizma@users.noreply.github.com>
2024-07-10 17:51:13 +0600

    Добавил проверку покрытия тестами, а так же пример теста для проверки работы ограничения на покрытие

---
+++ src/test/java/io/deeplay/camp/MainTest.java
@@ -0,0 +1,12 @@
+package io.deeplay.camp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    public void testExample() {
+        assertDoesNotThrow(Main::new);
+    }
+}
