e01321e706d028372dc0d3aca01b692f5e1f1b6d
Darling <92879479+DarlingInSteam@users.noreply.github.com>
2024-07-12 03:13:08 +0700

    feat: added logger template and added example of using logger

--- src/main/java/io/deeplay/camp/Main.java
+++ src/main/java/io/deeplay/camp/Main.java
@@ -1,13 +1,22 @@
 package io.deeplay.camp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
+        logger.info("Application started");
+        logger.debug("This is a debug message");
+        logger.error("error msg");
+        logger.trace("trace msg");
+        logger.warn("warn msg");
+        
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
