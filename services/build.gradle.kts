bb6b6806711f82dba663914939af54b3d76fb3f4
Darling <92879479+DarlingInSteam@users.noreply.github.com>
2024-07-11 07:55:28 +0700

    feat: added modules and included logback and junit

---
+++ services/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java")
+}
+
+group = "io.deeplay.camp"
+version = "unspecified"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("ch.qos.logback:logback-classic:1.4.12")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
