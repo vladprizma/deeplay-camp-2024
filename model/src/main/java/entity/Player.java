62a825f89dee921b13856964a9c416b7ad8f52c1
kaylebee <andreisheniatskii@gmail.com>
2024-07-14 14:34:51 +0700

    feature: added Game and GameLogic.

--- model/src/main/java/entity/Player.java
+++ model/src/main/java/entity/Player.java
@@ -1,4 +1,32 @@
 package entity;
 
+import enums.PlayerType;
+
 public class Player {
+    private String id;
+    private String color;
+    private PlayerType isAI;
+
+    // Конструктор для создания игрока
+    public Player(String id, String color, PlayerType isAI) {
+        this.id = id;
+        this.color = color;
+        this.isAI = isAI;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public PlayerType isAI() {
+        return isAI;
+    }
+
+    public void setAI(PlayerType isAI) {
+        this.isAI = isAI;
+    }
 }
--- model/src/main/java/enums/PlayerType.java
+++ model/src/main/java/enums/PlayerType.java
@@ -1,4 +1,6 @@
 package enums;
 
 public enum PlayerType {
+    HUMAN,
+    BOT
 }
