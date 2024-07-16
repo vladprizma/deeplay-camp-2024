958c59194ae629fcd665d43638d244f48dc3f008
Darling <92879479+DarlingInSteam@users.noreply.github.com>
2024-07-16 20:47:19 +0700

    feat: add log info for bad ip and port

--- src/main/java/io/deeplay/camp/Main.java
+++ src/main/java/io/deeplay/camp/Main.java
@@ -12,7 +12,7 @@ import java.util.concurrent.*;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-//    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
     private static String serverIp;
     private static int serverPort;
     
@@ -23,5 +23,6 @@ public class Main {
             serverIp = properties.getProperty("server.ip");
             serverPort = Integer.parseInt(properties.getProperty("server.port"));
         }
+        else logger.error("Не удалось запустить сервер. Проверьте логи, чтобы найти причину.");
     }
 }
