d20dba564957bfb0f34518a74032307bf59f0e82
kaylebee <andreisheniatskii@gmail.com>
2024-07-18 22:15:43 +0700

    refactor: Chip and Tile less Color field.

--- model/src/test/java/TileTest.java
+++ model/src/test/java/TileTest.java
@@ -6,27 +6,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TileTest {
 
     @Test
-    public void testGetColor() {
-        Tile tile = new Tile(Color.WHITE, 0, 0);
-        assertEquals(Color.WHITE, tile.getColor());
-    }
-
-    @Test
-    public void testSetColor() {
-        Tile tile = new Tile(Color.WHITE, 0, 0);
-        tile.setColor(Color.BLACK);
-        assertEquals(Color.BLACK, tile.getColor());
-    }
-
-    @Test
     public void testGetX() {
-        Tile tile = new Tile(Color.YELLOW, 3, 0);
+        Tile tile = new Tile(3, 0);
         assertEquals(3, tile.getX());
     }
 
     @Test
     public void testGetY() {
-        Tile tile = new Tile(Color.YELLOW, 0, 5);
+        Tile tile = new Tile(0, 5);
         assertEquals(5, tile.getY());
     }
 }
