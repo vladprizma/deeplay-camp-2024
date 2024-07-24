8eb3aecab3b97a2711db8578df2fcd81faabf308
Darling <92879479+DarlingInSteam@users.noreply.github.com>
2024-07-25 02:04:57 +0700

    added jwt service and PasswordService

--- services/build.gradle.kts
+++ services/build.gradle.kts
@@ -16,6 +16,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("ch.qos.logback:logback-classic:1.4.12")
+    implementation("org.mindrot:jbcrypt:0.4")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly ("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly ("io.jsonwebtoken:jjwt-jackson:0.11.5")
 }
 
 tasks.test {
