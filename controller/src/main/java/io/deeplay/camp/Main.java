1fa7d2a46be311ce20931ba0c88812f9bef7cb29
Darling <92879479+DarlingInSteam@users.noreply.github.com>
2024-08-09 02:10:39 +0700

    add docs for move

--- controller/src/main/java/io/deeplay/camp/Main.java
+++ controller/src/main/java/io/deeplay/camp/Main.java
@@ -10,7 +10,7 @@ public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
     private static final String SERVER_ADDRESS = "localhost";
     private static final int SERVER_PORT = 8080;
-
+    
     public static void main(String[] args) {
         try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
              BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
