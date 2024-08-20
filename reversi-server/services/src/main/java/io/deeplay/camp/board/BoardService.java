3633de44d81b9853ef670511493d2b5bddf3ef8d
Vitamin <viopolshkova@gmail.com>
2024-08-20 09:01:45 +0600

    added NoPlayerMethods to BoardService

--- reversi-server/services/src/main/java/io/deeplay/camp/board/BoardService.java
+++ reversi-server/services/src/main/java/io/deeplay/camp/board/BoardService.java
@@ -14,11 +14,13 @@ public class BoardService {
     private long whiteChips;
     private long blackValidMoves;
     private long whiteValidMoves;
+    private int playerID;
 
     public BoardService(Board board) {
         this.board = board;
         this.blackChips = board.getBlackChips();
         this.whiteChips = board.getWhiteChips();
+        playerID = 1;
     }
 
     //!! Установка фишки на доску
@@ -151,6 +153,140 @@ public class BoardService {
                 board.setWhiteChips(whiteChips);
                 board.setBlackChips(blackChips);
             }
+            playerSwap();
+        }
+    }
+
+    public void setPieceNoPlayer(int x, int y) {
+        long piece = 1L << (x + 8 * y);
+        long tempChips;
+        long currentChips = 0; long invertedChips = 0;
+        int a; int b;
+        if (playerID == 1) {
+            currentChips = blackChips;
+            invertedChips = whiteChips;
+        } else if (playerID == 2) {
+            currentChips = whiteChips;
+            invertedChips = blackChips;
+        } else System.out.println("Who are moving?");
+
+        if (isValidMoveNoPlayer(x, y)) {
+            currentChips |= piece;
+            // Горизонтали
+            if(x > 0){
+                tempChips = 0;
+                for (int i = x - 1; i >= 0; i--) {
+                    if ((invertedChips & (1L << (i + 8 * y))) != 0) {
+                        tempChips |= (1L << (i + 8 * y));
+                    } else if ((currentChips & (1L << (i + 8 * y))) != 0) {
+                        invertedChips &= ~tempChips;
+                        currentChips |= tempChips;
+                        break;
+                    } else break;
+                }
+            }
+            if(x < 7){
+                tempChips = 0;
+                for (int i = x + 1; i < 8; i++) {
+                    if ((invertedChips & (1L << (i + 8 * y))) != 0) {
+                        tempChips |= (1L << (i + 8 * y));
+                    } else if ((currentChips & (1L << (i + 8 * y))) != 0) {
+                        invertedChips &= ~tempChips;
+                        currentChips |= tempChips;
+                        break;
+                    } else break;
+                }
+            }
+            // Вертикали
+            if(y > 0){
+                tempChips = 0;
+                for (int j = y - 1; j >= 0; j--) {
+                    if ((invertedChips & (1L << (x + 8 * j))) != 0) {
+                        tempChips |= (1L << (x + 8 * j));
+                    } else if ((currentChips & (1L << (x + 8 * j))) != 0) {
+                        invertedChips &= ~tempChips;
+                        currentChips |= tempChips;
+                        break;
+                    } else break;
+                }
+            }
+            if(y < 7){
+                tempChips = 0;
+                for (int j = y + 1; j < 8; j++) {
+                    if ((invertedChips & (1L << (x + 8 * j))) != 0) {
+                        tempChips |= (1L << (x + 8 * j));
+                    } else if ((currentChips & (1L << (x + 8 * j))) != 0) {
+                        invertedChips &= ~tempChips;
+                        currentChips |= tempChips;
+                        break;
+                    } else break;
+                }
+            }
+            // Диагонали
+            if(y > 0 && x > 0){
+                tempChips = 0; a = x - 1;
+                for (int j = y - 1; j >= 0; j--) {
+                    if ((invertedChips & (1L << (a + 8 * j))) != 0 && a >= 0) {
+                        tempChips |= (1L << (a + 8 * j));
+                        a--;
+                    } else if ((currentChips & (1L << (a + 8 * j))) != 0 && a >= 0) {
+                        invertedChips &= ~tempChips;
+                        currentChips |= tempChips;
+                        break;
+                    } else break;
+                }
+            }
+            if(y > 0 && x < 7){
+                tempChips = 0; b = x + 1;
+                for (int j = y - 1; j >= 0; j--) {
+                    if ((invertedChips & (1L << (b + 8 * j))) != 0) {
+                        tempChips |= (1L << (b + 8 * j));
+                        b++;
+                    } else if ((currentChips & (1L << (b + 8 * j))) != 0) {
+                        invertedChips &= ~tempChips;
+                        currentChips |= tempChips;
+                        break;
+                    } else break;
+                }
+            }
+            if(y < 7 && x > 0){
+                tempChips = 0; a = x - 1;
+                for (int j = y + 1; j < 8; j++) {
+                    if ((invertedChips & (1L << (a + 8 * j))) != 0) {
+                        tempChips |= (1L << (a + 8 * j));
+                        a--;
+                    } else if ((currentChips & (1L << (a + 8 * j))) != 0) {
+                        invertedChips &= ~tempChips;
+                        currentChips |= tempChips;
+                        break;
+                    } else break;
+                }
+            }
+            if(y < 7 && x < 7){
+                tempChips = 0; b = x + 1;
+                for (int j = y + 1; j < 8; j++) {
+                    if ((invertedChips & (1L << (b + 8 * j))) != 0) {
+                        tempChips |= (1L << (b + 8 * j));
+                        b++;
+                    } else if ((currentChips & (1L << (b + 8 * j))) != 0) {
+                        invertedChips &= ~tempChips;
+                        currentChips |= tempChips;
+                        break;
+                    } else break;
+                }
+            }
+            if (playerID == 1) {
+                blackChips = currentChips;
+                whiteChips = invertedChips;
+                board.setWhiteChips(whiteChips);
+                board.setBlackChips(blackChips);
+            } else {
+                whiteChips = currentChips;
+                blackChips = invertedChips;
+                board.setWhiteChips(whiteChips);
+                board.setBlackChips(blackChips);
+            }
+            playerSwap();
         }
     }
 
@@ -183,6 +319,17 @@ public class BoardService {
         }
     }
 
+    public boolean isValidMoveNoPlayer(int x, int y) {
+        createValidMoves();
+        if (playerID == 1) {
+            return (blackValidMoves & (1L << (x + 8 * y))) != 0;
+        } else if (playerID == 2) {
+            return (whiteValidMoves & (1L << (x + 8 * y))) != 0;
+        } else {
+            return false;
+        }
+    }
+
     // Получение возможных вариантов
     public long getValidMoves(int player) {
         createValidMoves();
@@ -190,6 +337,17 @@ public class BoardService {
         if (player == 1) {
             validMoves |= blackValidMoves;
         } else if (player == 2) {
+            validMoves |= whiteValidMoves;
+        }
+        return validMoves;
+    }
+
+    public long getValidMovesNoPlayer() {
+        createValidMoves();
+        long validMoves = 0x0000000000000000L;
+        if (playerID == 1) {
+            validMoves |= blackValidMoves;
+        } else if (playerID == 2) {
             validMoves |= whiteValidMoves;
         }
         return validMoves;
@@ -347,6 +505,46 @@ public class BoardService {
         return state;
     }
 
+    public StringBuilder getBoardState() {
+        createValidMoves();
+        StringBuilder state = new StringBuilder();
+        state.append("\n\n");
+        for (int y = 0; y < 8; y++) {
+            state.append((8 - y) + " ");
+            for (int x = 0; x < 8; x++) {
+                if (hasPiece(x, y)) {
+                    if ((blackChips & (1L << (x + 8 * y))) != 0) {
+                        state.append("X ");
+                    } else {
+                        state.append("0 ");
+                    }
+                } else {
+                    long validMoves = (playerID == 1) ? blackValidMoves : whiteValidMoves;
+                    long mask = 1L << (x + 8 * y);
+                    if ((validMoves & mask) != 0) {
+                        state.append("* ");
+                    } else {
+                        state.append(". ");
+                    }
+                }
+            }
+            state.append("\n");
+        }
+        state.append(" ");
+        for (char i = 'a'; i <='h'; i++)
+            state.append(" " + i);
+
+        state.append("\n\n");
+        return state;
+    }
+
+    //Смена игрока
+    public void playerSwap(){
+        if(playerID == 1) {
+            playerID = 2;
+        } else playerID = 1;
+    }
+
     public String getBoardStateDTO(int player){
         StringBuilder state = new StringBuilder("");
         for (int y = 0; y < 8; y++) {
@@ -359,6 +557,31 @@ public class BoardService {
                     }
                 } else {
                     long validMoves = (player == 1) ? blackValidMoves : whiteValidMoves;
+                    long mask = 1L << (x + 8 * y);
+                    if ((validMoves & mask) != 0) {
+                        state.append("* ");
+                    } else {
+                        state.append(". ");
+                    }
+                }
+            }
+        }
+
+        return "Board{" + state + '}';
+    }
+
+    public String getBoardStateDTOnoPlayer(){
+        StringBuilder state = new StringBuilder("");
+        for (int y = 0; y < 8; y++) {
+            for (int x = 0; x < 8; x++) {
+                if (hasPiece(x, y)) {
+                    if ((blackChips & (1L << (x + 8 * y))) != 0) {
+                        state.append("X ");
+                    } else {
+                        state.append("0 ");
+                    }
+                } else {
+                    long validMoves = (playerID == 1) ? blackValidMoves : whiteValidMoves;
                     long mask = 1L << (x + 8 * y);
                     if ((validMoves & mask) != 0) {
                         state.append("* ");
@@ -401,9 +624,9 @@ public class BoardService {
         return whiteValidMoves;
     }
 
-    public List<Tile> getAllValidTiles(int playerId) {
+    public List<Tile> getAllValidTiles(int player) {
         List<Tile> validTiles = new ArrayList<>();
-        long validMoves = getValidMoves(playerId);
+        long validMoves = getValidMoves(player);
         for (int i = 0; i < 64; i++) {
             long mask = 1L << i;
             if ((validMoves & mask) != 0) {
@@ -415,18 +638,58 @@ public class BoardService {
         return validTiles;
     }
 
-    public boolean makeMove(int playerId, Tile tile) {
+    public List<Tile> getAllValidTileNoPlayer() {
+        List<Tile> validTiles = new ArrayList<>();
+        long validMoves = getValidMoves(playerID);
+        for (int i = 0; i < 64; i++) {
+            long mask = 1L << i;
+            if ((validMoves & mask) != 0) {
+                int x = i % 8;
+                int y = i / 8;
+                validTiles.add(new Tile(x, y));
+            }
+        }
+        return validTiles;
+    }
+
+    public boolean makeMove(int player, Tile tile) {
         int x = tile.getX();
         int y = tile.getY();
-        setPiece(x, y, playerId);
+        setPiece(x, y, player);
 
         return true;
     }
 
-    public List<Tile> getChips(int playerId) {
+    public boolean makeMoveNoPlayer(Tile tile) {
+        int x = tile.getX();
+        int y = tile.getY();
+        setPieceNoPlayer(x, y);
+
+        return true;
+    }
+
+
+    public List<Tile> getChips(int player) {
         List<Tile> playerChips = new ArrayList<>();
         long chips;
-        if(playerId == 1) {
+        if(player == 1) {
+            chips = blackChips;
+        } else chips = whiteChips;
+        for (int i = 0; i < 64; i++) {
+            long mask = 1L << i;
+            if ((chips & mask) != 0) {
+                int x = i % 8;
+                int y = i / 8;
+                playerChips.add(new Tile(x, y));
+            }
+        }
+        return playerChips;
+    }
+
+    public List<Tile> getChipsNoPlayer() {
+        List<Tile> playerChips = new ArrayList<>();
+        long chips;
+        if(playerID == 1) {
             chips = blackChips;
         } else chips = whiteChips;
         for (int i = 0; i < 64; i++) {
@@ -468,6 +731,7 @@ public class BoardService {
         this.whiteChips = oldBoardService.whiteChips;
         this.blackValidMoves = oldBoardService.blackValidMoves;
         this.whiteValidMoves = oldBoardService.whiteValidMoves;
+        this.playerID = oldBoardService.playerID;
     }
 
     public BoardService getCopy() {
