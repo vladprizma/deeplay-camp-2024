50781ac1c4f1191ff3a052c4a30ba397e13597e5
Darling <92879479+DarlingInSteam@users.noreply.github.com>
2024-08-27 14:50:36 +0700

    change maven to gradle kts

---
+++ reversi-server/metrics/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.3.3"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "io.deeplay.camp"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-influx")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
--- reversi-server/metrics/src/main/java/io/deeplay/camp/metrics/MetricsApplication.java
+++ reversi-server/metrics/src/main/java/io/deeplay/camp/metrics/MetricsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MetricsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MetricsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MetricsApplication.class, args);
+    }
 
 }
