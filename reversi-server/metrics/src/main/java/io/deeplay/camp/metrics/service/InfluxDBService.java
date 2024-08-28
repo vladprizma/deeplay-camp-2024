5f62288f5ac6d8ac62f733eae378de5342b0b4db
Darling <92879479+DarlingInSteam@users.noreply.github.com>
2024-08-28 18:50:39 +0700

    add metrics functions in metrics service

--- reversi-server/metrics/src/main/java/io/deeplay/camp/metrics/service/InfluxDBService.java
+++ reversi-server/metrics/src/main/java/io/deeplay/camp/metrics/service/InfluxDBService.java
@@ -6,7 +6,6 @@ import org.influxdb.dto.Point;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.awt.*;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -22,15 +21,61 @@ public class InfluxDBService {
         this.influxDB.setDatabase("influxDB");
     }
 
-    public void insertRandomTemperature() {
-        int temperature = 15 + (int) (Math.random() * (40 - 15 + 1));
-
-        Point point = Point.measurement("temperature")
+    public void insertResponseTime(long responseTime, int totalUsers, int serverId) {
+        Point point = Point.measurement("response_time")
                 .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
-                .addField("value", temperature)
-                .tag("location", "room1") 
+                .addField("value", responseTime)
+                .addField("total_users", totalUsers)
+                .addField("server_id", serverId)
                 .build();
+        influxDB.write(point);
+    }
 
+    public void insertThroughput(int throughput, int serverId) {
+        Point point = Point.measurement("throughput")
+                .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+                .addField("value", throughput)
+                .addField("server_id", serverId)
+                .build();
+        influxDB.write(point);
+    }
+
+    public void insertErrorRate(double errorRate, int serverId) {
+        Point point = Point.measurement("error_rate")
+                .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+                .addField("value", errorRate)
+                .addField("server_id", serverId)
+                .build();
+        influxDB.write(point);
+    }
+
+    public void insertGameSessionsCount(int gameSessionsCount, int serverId) {
+        Point point = Point.measurement("game_sessions_count")
+                .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+                .addField("value", gameSessionsCount)
+                .addField("server_id", serverId)
+                .build();
+        influxDB.write(point);
+    }
+
+    public void insertBotResponseTime(long botResponseTime, int botDepth, int serverId) {
+        Point point = Point.measurement("bot_response_time")
+                .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+                .addField("response_time", botResponseTime)
+                .addField("bot_depth", botDepth)
+                .addField("server_id", serverId)
+                .build();
+        influxDB.write(point);
+    }
+
+    public void insertServerLoad(double cpuUsage, double memoryUsage, double diskUsage, int serverId) {
+        Point point = Point.measurement("server_load")
+                .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+                .addField("cpu_usage", cpuUsage)
+                .addField("memory_usage", memoryUsage)
+                .addField("disk_usage", diskUsage)
+                .addField("server_id", serverId)
+                .build();
         influxDB.write(point);
     }
 }
