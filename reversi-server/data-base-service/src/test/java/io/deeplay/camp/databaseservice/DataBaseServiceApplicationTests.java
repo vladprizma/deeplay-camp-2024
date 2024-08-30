d3b1cb6bbc9fd476ffb98452eead86758222e98e
Darling <92879479+DarlingInSteam@users.noreply.github.com>
2024-08-30 23:31:11 +0700

    add others files for data-base-service

---
+++ reversi-server/data-base-service/src/test/java/io/deeplay/camp/databaseservice/DataBaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.deeplay.camp.databaseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataBaseServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
