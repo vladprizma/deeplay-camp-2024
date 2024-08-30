67380bfd9e698b9c96ac368ac351eb78e704f7b8
Darling <92879479+DarlingInSteam@users.noreply.github.com>
2024-08-30 20:01:22 +0700

    add other files for service

---
+++ reversi-server/bot-factory/src/main/java/io/deeplay/camp/botfactory/BotFactoryApplication.java
@@ -0,0 +1,13 @@
+package io.deeplay.camp.botfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotFactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotFactoryApplication.class, args);
+    }
+
+}
