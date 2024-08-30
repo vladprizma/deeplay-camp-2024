7ef143fa69af6ee6c1b8dfc855d505546c1a8f46
Darling <92879479+DarlingInSteam@users.noreply.github.com>
2024-08-30 23:31:11 +0700

    add others files for data-base-service

---
+++ reversi-server/data-base-service/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.3.3"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "io.deeplay.camp"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("org.postgresql:postgresql")
+    implementation("jakarta.persistence:jakarta.persistence-api:3.0.0")
+    implementation("org.hibernate:hibernate-core:6.2.5.Final")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ reversi-server/data-base-service/src/main/java/io/deeplay/camp/databaseservice/DataBaseServiceApplication.java
@@ -0,0 +1,13 @@
+package io.deeplay.camp.databaseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBaseServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataBaseServiceApplication.class, args);
+    }
+
+}
